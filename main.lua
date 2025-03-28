1198a29c2415be95c9800f3b4ab58ba1562dd47f
Ohyeahboomboomalright12 <110528901+Ohyeahboomboomalright12@users.noreply.github.com>
2025-03-29 00:42:15 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,40 +2,35 @@ local WindUI = loadstring(game:HttpGet("https://tree-hub.vercel.app/api/UI/WindU
 local Window = WindUI:CreateWindow({
     Title = "Strix Hub", -- UI Title
     Icon = "strikethrough", -- Url or rbxassetid or lucide
-    Author = "ns", -- Author & Creator
+    Author = "ns and ui by .ftgs", -- Author & Creator
     Folder = "CloudHub", -- Folder name for saving data (And key)
     Size = UDim2.fromOffset(580, 460), -- UI Size
     KeySystem = { -- Creates key system
         Key = "sigmaballs", -- key
-        Note = "Join the Discord!", -- Note
+        Note = "Join the discord!", -- Note
         URL = "https://discord.gg/vsauztQYNX", -- URL To get key (example: Discord)
-        SaveKey = false, -- Saves the key in the folder specified above
+        SaveKey = true, -- Saves the key in the folder specified above
     }, 
     Transparent = true,-- UI Transparency
     Theme = "Dark", -- UI Theme
     SideBarWidth = 170, -- UI Sidebar Width (number)
     HasOutline = true, -- Adds Outlines to the window
-
-	
 })
-
-local Dialog = Window:Dialog({
-    Icon = "droplet", -- lucide
-    Title = "Welcome",
-    Content = "Thanks for using my script",
-    Buttons = {
-        {
-            Title = "OK!", 
-            Callback = function() end, 
-            Variant = "Primary"
-        }
-    }
+Window:EditOpenButton({
+    Title = "Open Example UI", -- Title
+    Icon = "monitor", -- Icon
+    CornerRadius = UDim.new(0,10), -- Radius
+    StrokeThickness = 2, -- Stroke Thickness
+    Color = ColorSequence.new( -- Gradient
+        Color3.fromHex("FF0F7B"), 
+        Color3.fromHex("F89B29")
+    ),
+    Position = UDim2.new(0.5,0,0.5,0), -- Position
+    Enabled = true,   -- true or false
+    Draggable = true, -- true or false
 })
-
--- important
-Dialog:Open()
-
-local Tab = Window:Tab({
-    Title = "Universal",
-    Icon = "globe", -- lucide or rbxassetid
+local Notification = WindUI:Notify({
+    Title = "Notification",
+    Content = "Content",
+    Duration = 5,
 })
