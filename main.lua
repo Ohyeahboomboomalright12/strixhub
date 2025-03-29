0daacc45b141e7634d7865fcb4ac14d6d1a12dc4
Ohyeahboomboomalright12 <110528901+Ohyeahboomboomalright12@users.noreply.github.com>
2025-03-29 12:34:25 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,17 +2,70 @@ local WindUI = loadstring(game:HttpGet("https://tree-hub.vercel.app/api/UI/WindU
 local Window = WindUI:CreateWindow({
     Title = "Strix Hub", -- UI Title
     Icon = "strikethrough", -- Url or rbxassetid or lucide
-    Author = "ns and ui by .ftgs", -- Author & Creator
+    Author = "kyu112_", -- Author & Creator
     Folder = "CloudHub", -- Folder name for saving data (And key)
     Size = UDim2.fromOffset(580, 460), -- UI Size
     KeySystem = { -- Creates key system
         Key = "sigmaballs", -- key
-        Note = "Join the discord!", -- Note
-        URL = "https://discord.gg/vsauztQYNX", -- URL To get key (example: Discord)
-        SaveKey = true, -- Saves the key in the folder specified above
+        Note = "join the discord god dammit ", -- Note
+        URL = "https://github.com/Footagesus/WindUI", -- URL To get key (example: Discord)
+        SaveKey = false, -- Saves the key in the folder specified above
     }, 
     Transparent = true,-- UI Transparency
     Theme = "Dark", -- UI Theme
     SideBarWidth = 170, -- UI Sidebar Width (number)
     HasOutline = true, -- Adds Outlines to the window
+})
+WindUI:SetTheme("Halloween")
+
+local Tab = Window:Tab({
+    Title = "Universal",
+    Icon = "globe", -- lucide or rbxassetid
+})
+
+local Section = Tab:Section({ 
+    Title = "Universal Scripts",
+    TextXAlignment = "Left",
+    TextSize = 17, -- Default Size
+})
+
+local Paragraph = Tab:Paragraph({
+    Title = "Infinite Yield",
+    Desc = "A sophisticated command hub",
+    Image = "droplet", -- lucide or URL or rbxassetid://
+    ImageSize = 20,
+    Buttons = {
+        {
+            Title = "Execute",
+            Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() print("Executed!") end
+        },
+     
+    }
+})
+
+local Paragraph = Tab:Paragraph({
+    Title = "Dex Explorer",
+    Desc = "Roblox Studio's Explorer tab but in normal roblox",
+    Image = "droplet", -- lucide or URL or rbxassetid://
+    ImageSize = 20,
+    Buttons = {
+        {
+            Title = "Execute",
+            Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))() print("Executed!") end
+        },
+     
+    }
+})
+local Paragraph = Tab:Paragraph({
+    Title = "Sirius",
+    Desc = "gives you infinite aura",
+    Image = "trophy", -- lucide or URL or rbxassetid://
+    ImageSize = 20,
+    Buttons = {
+        {
+            Title = "Execute",
+            Callback = function() loadstring(game:HttpGet('https://sirius.menu/script'))() print("Executed!") end
+        },
+     
+    }
 })
